7db20746a9be8b0b36b998e61b4c6e343a64a6ac
Carlos Saca Jr. <carlossacajr.@Mac.fritz.box>
2026-05-21 16:51:31 +0200

    feat: add departments_schedule to backup sync system 3

--- src/services/dexie/departments_schedule.ts
+++ src/services/dexie/departments_schedule.ts
@@ -1,6 +1,19 @@
 import appDb from '@db/appDb';
 import { DeptWeekType } from '@definition/departments_schedule';
 
+const dbUpdateSchedulesMetadata = async () => {
+  const metadata = await appDb.metadata.get(1);
+
+  if (!metadata) return;
+
+  metadata.metadata.schedules = {
+    ...metadata.metadata.schedules,
+    send_local: true,
+  };
+
+  await appDb.metadata.put(metadata);
+};
+
 export const dbDeptScheduleGet = async (weekOf: string) => {
   const schedule = await appDb.departments_schedule.get(weekOf);
   return schedule || null;
@@ -8,10 +21,12 @@ export const dbDeptScheduleGet = async (weekOf: string) => {
 
 export const dbDeptScheduleSave = async (data: DeptWeekType) => {
   await appDb.departments_schedule.put(data);
+  await dbUpdateSchedulesMetadata();
 };
 
 export const dbDeptScheduleBulkSave = async (data: DeptWeekType[]) => {
   await appDb.departments_schedule.bulkPut(data);
+  await dbUpdateSchedulesMetadata();
 };
 
 export const dbDeptScheduleGetAll = async () => {
